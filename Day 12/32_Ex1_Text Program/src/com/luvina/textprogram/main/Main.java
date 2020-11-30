682f981917a3716a13110a62e6ca15c5ec8b62f7
Hoang Anh <legendarycaptains@gmail.com>
2020-11-30 22:15:45 +0700

    edit src folder in Day 12

---
+++ Day 12/32_Ex1_Text Program/src/com/luvina/textprogram/main/Main.java
@@ -0,0 +1,19 @@
+/**
+ * @Project_Name 32_Ex1_Text Program
+ * @author Hoang Anh
+ * @date 30 thg 11, 2020
+ * @version 1.0
+ */
+package com.luvina.textprogram.main;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Day 12/32_Ex1_Text Program/src/com/luvina/textprogram/text/Text.java
@@ -0,0 +1,21 @@
+/**
+ * @Project_Name 32_Ex1_Text Program
+ * @author Hoang Anh
+ * @date 30 thg 11, 2020
+ * @version 1.0
+ */
+package com.luvina.textprogram.text;
+
+public class Text {
+	private String text;
+	
+	/**
+	 * Phương thức khởi tạo của đối tượng Text
+	 */
+	public Text(String text) {
+		this.text = text;
+	}
+	
+	
+	
+}
